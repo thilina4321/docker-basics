90142df29fb993b3fa688aa0536a61ecf6c9db53
Thilina Dilshan <38967633+thilina4321@users.noreply.github.com>
2022-10-21 07:28:50 +0530

    Apply suggestions from code review

--- frontend/src/components/goals/GoalItem.js
+++ frontend/src/components/goals/GoalItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './GoalItem.css';
 
-function GoalItem(props) {
+const GoalItem = (props)=> {
   return <li className="goal-item" onClick={props.onDelete.bind(null, props.id)}>{props.text}</li>;
 }
 
